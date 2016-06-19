7d78244e6aaabf43a10c955c0403d07d49df4be3
lilo <lilo@squareup.com>
2016-06-19 04:55:15 -0700

    remove template pink email button

--- app/src/main/java/com/example/lilo/todoapp/MainActivity.java
+++ app/src/main/java/com/example/lilo/todoapp/MainActivity.java
@@ -39,17 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     public void populateArrayItems() {
